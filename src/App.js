4cb35913e37ad02192964141f83936ad550dd955
kulaszewicz <s17838@pjwstk.edu.pl>
2020-01-24 23:41:04 +0100

    Init project setup + main App page

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Typography } from "@material-ui/core";
+
+const useStyles = makeStyles({
+    App: {
+        height: '100vh',
+        position: 'relative',
+        backgroundImage: 'url("http://fotopolska.eu/foto/300/300047.jpg")',
+        opacity: '0.7',
+        backgroundColor: 'green',
+    },
+    head: {
+        fontSize: '72px',
+        background: 'linear-gradient(to right, #30CFD0 0%, #330867 100%)',
+        backgroundClip: 'text',
+        webkitTextFillColor: 'transparent',
+        fontWeight: '700',
+        textAlign: 'center',
+        '-webkit-background-clip': 'text',
+        '-webkit-text-fill-color': 'transparent',
+        textShadow: '4px 4px #ff00ff',
+    },
+    Overlay: {
+        background: 'linear-gradient(to right, #30CFD0 0%, #330867 100%)',
+        position: 'absolute',
+        top: '0',
+        left: '0',
+        width: '100vw',
+        height: '100vh',
+        opacity: '0.8'
+    }
+});
+
+const App = () => {
+    const styles = useStyles();
+
+    console.log(styles);
+    return (
+        <div className={styles.Overlay}>
+            <div className={styles.App}>
+                <Typography variant={"h1"} className={styles.head}>HALOOO MISNIA GDZIE JEST ZBYSNIA</Typography>
+            </div>
+        </div>
+    )
+};
+
+export default App;
+
+
